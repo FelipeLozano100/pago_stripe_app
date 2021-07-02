21470bd8067c49288901b021ae1770cb2a3b3ff0
felipelozano71@yahoo.com <mcfa9596>
2021-07-02 11:28:47 -0500

    fIN SECCION 23 DISEÑO PAGOS

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:stripe_app/data/tajetas.dart';
+import 'package:stripe_app/helpers/helpers.dart';
+import 'package:stripe_app/pages/tarjeta_page.dart';
 import 'package:stripe_app/widgets/total_pay_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -14,7 +16,12 @@ class HomePage extends StatelessWidget {
           actions: [
             IconButton(
               icon: Icon(Icons.add),
-              onPressed: () {},
+              onPressed: () async {
+                // mostrarLoading(context);
+                // await Future.delayed(Duration(seconds: 1));
+                // Navigator.pop(context);
+                mostrarAlerta(context, 'Hola', 'Mundo');
+              },
             )
           ],
         ),
@@ -33,15 +40,19 @@ class HomePage extends StatelessWidget {
                 itemBuilder: (_, i) {
                   final tarjeta = tarjetas[i];
                   return GestureDetector(
-                    onTap: (){
-                      print(tarjeta.cardHolderName);
+                    onTap: () {
+                      Navigator.push(
+                          context, navegarFadeIn(context, TarjetaPage()));
                     },
-                    child: CreditCardWidget(
-                      cardNumber: tarjeta.cardNumberHidden!,
-                      expiryDate: tarjeta.expiracyDate!,
-                      cardHolderName: tarjeta.cardHolderName!,
-                      cvvCode: tarjeta.cvv!,
-                      showBackView: false,
+                    child: Hero(
+                      tag: tarjeta.cardNumber!,
+                      child: CreditCardWidget(
+                        cardNumber: tarjeta.cardNumberHidden!,
+                        expiryDate: tarjeta.expiracyDate!,
+                        cardHolderName: tarjeta.cardHolderName!,
+                        cvvCode: tarjeta.cvv!,
+                        showBackView: false,
+                      ),
                     ),
                   );
                 },
