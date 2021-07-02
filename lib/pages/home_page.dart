2c485116185a993b21f1c9e99071213d8074819f
felipelozano71@yahoo.com <mcfa9596>
2021-07-02 11:00:13 -0500

    Diseño app pagos

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:stripe_app/data/tajetas.dart';
+import 'package:stripe_app/widgets/total_pay_button.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Pagar'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              width: size.width,
+              height: size.height,
+              top: 200,
+              child: PageView.builder(
+                controller: PageController(
+                  viewportFraction: 0.9,
+                ),
+                physics: BouncingScrollPhysics(),
+                itemCount: tarjetas.length,
+                itemBuilder: (_, i) {
+                  final tarjeta = tarjetas[i];
+                  return GestureDetector(
+                    onTap: (){
+                      print(tarjeta.cardHolderName);
+                    },
+                    child: CreditCardWidget(
+                      cardNumber: tarjeta.cardNumberHidden!,
+                      expiryDate: tarjeta.expiracyDate!,
+                      cardHolderName: tarjeta.cardHolderName!,
+                      cvvCode: tarjeta.cvv!,
+                      showBackView: false,
+                    ),
+                  );
+                },
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              child: TotalPayButton(),
+            ),
+          ],
+        ));
+  }
+}
